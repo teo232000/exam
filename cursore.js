d41894a8ce24b2119989bb1f145a7497fa906234
Teo <teodoropitu@gmail.com>
2023-09-14 01:56:09 +0200

    aggiunta scompara js da schermi più piccoli di una certa fascia

--- cursore.js
+++ cursore.js
@@ -1,13 +1,15 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const cursor = document.getElementById('cursor');
 
-    document.addEventListener('mousemove', (e) => {
-        cursor.style.transform = `translate(${e.clientX - cursor.clientWidth / 2}px, ${e.clientY - cursor.clientHeight / 2}px)`;
-    });
+   
+    if (window.innerWidth >= 760) {
+        document.addEventListener('mousemove', (e) => {
+            cursor.style.transform = `translate(${e.clientX - cursor.clientWidth / 2}px, ${e.clientY - cursor.clientHeight / 2}px)`;
+        });
 
-    document.addEventListener('scroll', () => {
-        const e = window.event;
-        cursor.style.transform = `translate(${e.clientX - cursor.clientWidth / 2}px, ${e.clientY + window.scrollY - cursor.clientHeight / 2}px)`;
-    });
+        document.addEventListener('scroll', () => {
+            const e = window.event;
+            cursor.style.transform = `translate(${e.clientX - cursor.clientWidth / 2}px, ${e.clientY + window.scrollY - cursor.clientHeight / 2}px)`;
+        });
+    }
 });
